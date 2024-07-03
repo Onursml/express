13435d5c5c80aa2b975fcbd8f538780b0bd4cc14
Onursml <crzyonur5@gmail.com>
2024-07-03 18:05:39 +0300

    tek

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Ana Sayfa Rotası
+app.get('/', (req, res) => {
+    res.send('Merhaba, Express!');
+});
+
+// Başka Bir Rota
+app.get('/about', (req, res) => {
+    res.send('Bu basit bir Express uygulaması örneğidir.');
+});
+
+// Sunucuyu Başlatma
+app.listen(port, () => {
+    console.log(`Sunucu http://localhost:${port} adresinde çalışıyor`);
+});
